9a8a30d42923ca0d234d9ea944bbe7e36f420c13
bossteve1 <stephen.nyabuto@student.moringaschool.com>
2022-10-01 16:14:35 +0300

    complete assignment

--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -1,14 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import YourBotArmy from "./YourBotArmy";
 import BotCollection from "./BotCollection";
 
 function BotsPage() {
   //start here with your code for step one
+  const [bots, setBots] = useState ([]);
+  const [botArmy, setBotArmy] = useState([]);
 
+  //fetching data
+
+  useEffect(() => {
+    fetch("http://localhost:8002/bots")
+    .then(response => response.json())
+    .then(bots => setBots(bots))
+  }, []);
+
+  function addBotToArmy(armyBot){
+    if(!botArmy.find(bot => bot === armyBot)){
+      const foundBot = bots.find(bot => bot === armyBot)
+
+      setBotArmy([...botArmy, foundBot])
+    }
+  }
+
+  function removeBotFromArmy(armyBot) {
+    const botArmyList = botArmy.filter((bot)=> bot !== armyBot)
+    setBotArmy(botArmyList)
+  }
+
+  function removeBotPermanently (armyBot) {
+    if(botArmy.find((bot) => bot === armyBot)){
+      const filterBots = bots.filter(bot => bot !== armyBot)
+      const filterBotArmy = botArmy.filter(bot => bot !== armyBot)
+
+      setBots(filterBots)
+      setBotArmy(filterBotArmy)
+
+      fetch(`http://localhost:8002/bots/${armyBot.id}`, {
+        method: "DELETE"
+      })
+    }
+  }
+  
   return (
     <div>
-      <YourBotArmy />
-      <BotCollection />
+      <YourBotArmy  botArmy={botArmy} removeBot={removeBotFromArmy} deleteBot= {removeBotPermanently} />
+      <BotCollection bots={bots}  addBot={addBotToArmy} deleteBot={removeBotPermanently}/>
     </div>
   )
 }
--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,13 +1,19 @@
 import React from "react";
+import BotCard from "./BotCard";
 
-function YourBotArmy() {
+function YourBotArmy ({botArmy, removeBot, deleteBot}) {
   //your bot army code here...
+
+
+
 
   return (
     <div className="ui segment inverted olive bot-army">
       <div className="ui five column grid">
         <div className="row bot-army-row">
-          {/*...and here...*/}
+          {botArmy.map((bot) => (
+          <BotCard key={bot.id} bot={bot} handleBot={removeBot} handleDeleteBot={deleteBot}/>
+          ))}
           Your Bot Army
         </div>
       </div>
